c6c16d7bba45e9996f04bd689dda7517082de2c0
Jason Kennaly <jkennaly@gmail.com>
2015-06-28 17:32:42 -0400

    Initial commit

---
+++ app/scripts/controllers/login.failed.username.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jbk on 10/5/14.
+ */
+'use strict';
+/**
+ * @ngdoc function
+ * @name ftGameTimeApp.controller:LoginFailedUsernameCtrl
+ * @description
+ * # LoginFailedUsernameCtrl
+ * Controller of the ftGameTimeApp
+ */
+angular.module('ftGameTimeApp')
+    .controller('LoginFailedUsernameCtrl', function ($scope) {
+    });
---
+++ app/scripts/controllers/login.forgot.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jbk on 10/5/14.
+ */
+'use strict';
+/**
+ * @ngdoc function
+ * @name ftGameTimeApp.controller:LoginForgotCtrl
+ * @description
+ * # LoginForgotCtrl
+ * Controller of the ftGameTimeApp
+ */
+angular.module('ftGameTimeApp')
+    .controller('LoginForgotCtrl', function ($scope) {
+    });
